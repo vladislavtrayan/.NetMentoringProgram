c7b03dce49c8be6338f4022b20b6a732756a522b
Uladzislau Trayan <Uladzislau_Trayan@epam.com>
2021-01-03 18:49:20 +0300

    Remove size argument.

--- Module8/list/Sudoku/Sudoku/Sudoku/Sudoku.cs
+++ Module8/list/Sudoku/Sudoku/Sudoku/Sudoku.cs
@@ -6,15 +6,15 @@ namespace Sudoku
     public class Sudoku
     {
         private static HashSet<int> validSet = new HashSet<int>(){1,2,3,4,5,6,7,8,9};
-        public static bool IsValid(int[,] solution, int size = 9)
+        public static bool IsValid(int[,] solution)
         {
             if(solution == null)
                 throw new ArgumentNullException();
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < solution.GetLength(0); i++)
             {
                 var rowSet = new HashSet<int>();
                 var columnSet = new HashSet<int>();
-                for (int j = 0; j < size; j++)
+                for (int j = 0; j < solution.GetLength(1); j++)
                 {
                     rowSet.Add(solution[i, j]);
                     columnSet.Add(solution[j, i]);
