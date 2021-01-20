a6f38b45cfa2ca31a4560a808f8f4459746413e7
Uladzislau Trayan <Uladzislau_Trayan@epam.com>
2021-01-21 02:06:48 +0300

    add negative tests

--- Module8/list/BinaryGap/BinaryGap/Tests/UnitTest1.cs
+++ Module8/list/BinaryGap/BinaryGap/Tests/UnitTest1.cs
@@ -9,11 +9,22 @@ namespace Tests
         [Theory]
         [InlineData(9, 2)]
         [InlineData(529, 4)]
-        [InlineData(1111, 0)]
+        [InlineData(15, 0)]
         [InlineData(1041, 5)]
-        public void Test1(int n, int expectedBinaryGap)
+        [InlineData(int.MaxValue - 1, 0)]
+        [InlineData(int.MaxValue, 0)]
+        public void TestOnPositiveNumbers(int n, int expectedBinaryGap)
         {
             Assert.Equal(expectedBinaryGap, BinarySolver.Solve(n));
+        }
+        
+        [Theory]
+        [InlineData(int.MinValue)]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void NegativeOrZeroNumbersTest(int n)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => BinarySolver.Solve(n));
         }
     }
 }
