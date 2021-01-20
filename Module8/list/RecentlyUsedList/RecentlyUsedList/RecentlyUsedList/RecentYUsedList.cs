a608741e7871e2a1b75ff60d33d5b3068a82f830
Uladzislau Trayan <Uladzislau_Trayan@epam.com>
2021-01-21 02:52:47 +0300

    add base functionallity

--- Module8/list/RecentlyUsedList/RecentlyUsedList/RecentlyUsedList/RecentYUsedList.cs
+++ Module8/list/RecentlyUsedList/RecentlyUsedList/RecentlyUsedList/RecentYUsedList.cs
@@ -1,25 +1,37 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace RecentlyUsedList
 {
     public class RecentYUsedList
     {
-        
-        public int Count { get; private set; }
+        private readonly List<string> _storage = new List<string>();
+        public int Count => _storage.Count;
+
         public void Add(string element)
         {
-            throw new NotImplementedException();
+            if (_storage.Contains(element))
+            {
+                _storage.Remove(element);
+            }
+            _storage.Add(element);
         }
         
         public string this[int index]
         {
             get
             {
-                throw new NotImplementedException();
+                if(index < 0)
+                    throw new IndexOutOfRangeException();
+                return _storage[Count - 1 - index];
             }
             set
             {
-                throw new NotImplementedException();
+                if(index < 0)
+                    throw new IndexOutOfRangeException();
+
+                _storage[Count - 1 - index] = value;
             }
         }
     }
